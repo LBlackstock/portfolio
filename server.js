af698a518c55baeae5c2d50c5d778f2b51e73df6
LoLo8 <lnblackstock@gmail.com>
2017-01-24 23:21:08 -0800

    fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname,"./client")));
 app.use(express.static(path.join(__dirname,"./node_modules")));
 
 // require("./server/config/mongoose.js");
-require("./server/config/routes.js")
+// require("./server/config/routes.js")
 
 //start the server
 app.listen(8000, function(){
