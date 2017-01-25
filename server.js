7c3d13cf21a3bfc051cc42342d5a37eb8bfb2a04
LoLo8 <lnblackstock@gmail.com>
2017-01-24 23:19:54 -0800

    fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 8000;
 app.use(express.static(path.join(__dirname,"./client")));
 app.use(express.static(path.join(__dirname,"./node_modules")));
 
-require("./server/config/mongoose.js");
+// require("./server/config/mongoose.js");
 require("./server/config/routes.js")
 
 //start the server
